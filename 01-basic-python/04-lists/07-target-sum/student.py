de567e7338dab7453d3314bbedec350cbe0737bd
Ferre Laridon <ferre.laridon@student.ucll.be>
2022-02-23 15:38:22 +0100

    23/2/2022

--- 01-basic-python/04-lists/07-target-sum/student.py
+++ 01-basic-python/04-lists/07-target-sum/student.py
@@ -1,1 +1,10 @@
 # Write your code here
+from operator import truediv
+
+
+def target_sum(xs, target):
+    for x in xs:
+        for y in xs:
+            if x+y==target: 
+                return True
+    return False
--- 01-basic-python/05-strings/01-format-time/student.py
+++ 01-basic-python/05-strings/01-format-time/student.py
@@ -1,1 +1,7 @@
 # Write your code here
+def format_time(h,m,s):
+    hstr = f'{h}'.rjust(2,'0')
+    mstr = f'{m}'.rjust(2,'0')
+    sstr = f'{s}'.rjust(2,'0')
+
+    return f'{hstr}:{mstr}:{sstr}'

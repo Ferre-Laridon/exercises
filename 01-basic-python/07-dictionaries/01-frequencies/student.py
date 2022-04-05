7186f08da29de92a60a6fb202d9e343ab0962101
Ferre Laridon <ferre.laridon@student.ucll.be>
2022-04-05 18:30:58 +0200

    basic-python afgewerkt

--- 01-basic-python/07-dictionaries/01-frequencies/student.py
+++ 01-basic-python/07-dictionaries/01-frequencies/student.py
@@ -1,1 +1,8 @@
 # Write your code here
+def frequencies(xs):
+    result = {}
+    for item in xs:
+        if item not in result:
+            result[item] = 0
+        result[item] += 1
+    return result
--- 01-basic-python/08-sets/02-remove-duplicates/student.py
+++ 01-basic-python/08-sets/02-remove-duplicates/student.py
@@ -1,1 +1,9 @@
 # Write your code here
+def remove_duplicates(xs):
+    result = []
+    found = set()
+    for x in xs:
+        if x not in found:
+            result.append(x)
+            found.add(x)
+    return result

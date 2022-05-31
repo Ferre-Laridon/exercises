3c34de1fc78ed634ac2e5960f2af4b39964d30ec
Ferre Laridon <ferre.laridon@student.ucll.be>
2022-05-31 17:01:51 +0200

    textwrap juist gemaakt

--- exam/textwrap/solution.py
+++ exam/textwrap/solution.py
@@ -1,7 +1,12 @@
-import re
+import textwrap
 
 
-with open("input.txt") as file:
-    with open("output.txt", 'w') as output:
-        for str in file:
-            output.write('\n'.join(line.strip() for line in re.findall(r'.{1,40}(?:\s+|$)', str)))
+# parse line from input.txt and split it into object in a list
+with open("input.txt", "r") as f:
+    line = f.readline()
+    wrap_list = textwrap.wrap(line, 40)
+
+# print lines to output
+with open("output.txt", "w") as f:
+    for line in wrap_list:
+        f.write(line + '\n')

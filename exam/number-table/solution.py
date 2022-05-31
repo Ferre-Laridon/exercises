fce7fb1f3cbb95804da4c311747d7d6c436649fb
Ferre Laridon <ferre.laridon@student.ucll.be>
2022-05-31 15:33:21 +0200

    number-table gemaakt

--- exam/number-table/solution.py
+++ exam/number-table/solution.py
@@ -1,1 +1,13 @@
-# Write your solution in this file
+output = ""
+for i in range(10001):
+    # calculate decimal, binary and hexadecimal values
+    dec = str(i)
+    binair = str(bin(i))
+    hexa = str(hex(i))
+
+    # make row
+    output += dec.rjust(10) + \
+        binair.rjust(20) + hexa.rjust(10) + '\n'
+
+with open('output.txt', 'w') as f:
+    f.write(output)
